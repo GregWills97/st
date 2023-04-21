39a251ca3f650b82a101658577e6562fb9b7caa1
Gregory Williams <gregwills85@gmail.com>
2023-04-21 11:39:37 -0600

    Changing copy/paste keybindings

--- config.def.h
+++ config.def.h
@@ -206,7 +206,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (ShiftMask|MODKEY)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -218,6 +218,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
